fe3124ff01add5433d7da31ea472b411500f44fc
Iker Sandí Trejo <ikersanditrejo@gmail.com>
2019-03-04 11:35:51 -0600

    Practica 6: Agregar Formulario

---
+++ Practica_6/Practica_6/FormAutorFirma1.vb
@@ -0,0 +1,13 @@
+﻿Public Class FormAutorFirma1
+
+    Private Sub Autor_Regresar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Autor_Regresar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Beep()
+        Autor_Nombre.Show()
+        Autor_Grado.Show()
+        Autor_Profesor.Show()
+    End Sub
+End Class
---
+++ Practica_6/Practica_6/Inicio.vb
@@ -0,0 +1,10 @@
+﻿Public Class Inicio
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        FormAutorFirma1.Show()
+    End Sub
+End Class
